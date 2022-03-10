4184000f4f45dcb1869f894e7fc77d3110dc45d4
unknown <frakels1995@gmail.com>
2022-03-10 15:09:43 +0100

    add folder enteries

---
+++ lib/locator.dart
@@ -0,0 +1,12 @@
+import 'package:facial_recognition/services/camera.service.dart';
+import 'package:facial_recognition/services/ml_service.dart';
+import 'package:facial_recognition/services/face_detector_service.dart';
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.instance;
+
+void setupServices() {
+  locator.registerLazySingleton<CameraService>(() => CameraService());
+  locator.registerLazySingleton<FaceDetectorService>(() => FaceDetectorService());
+  locator.registerLazySingleton<MLService>(() => MLService());
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:facial_recognition/locator.dart';
+import 'package:facial_recognition/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  setupServices();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
